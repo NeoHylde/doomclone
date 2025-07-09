ad50a43d6854a4481044cd1736b05e4aab209965
Neo <neo.hyldelund@gmail.com>
2025-07-09 16:58:33 -0700

    Corridor component

---
+++ headers/Corridor.h
@@ -0,0 +1,26 @@
+#ifndef CORRIDOR_CLASS_H
+#define CORRIDOR_CLASS_H
+
+#include"Tile.h"
+#include"Wall.h"
+#include"shaderClass.h"
+#include"Mesh.h"
+#include"Camera.h"
+#include"MapComponent.h"
+
+#include<vector>
+#include<glm/glm.hpp>
+
+class Corridor : public MapComponent {
+public:
+    //std::vector<Tile> tiles;
+    Tile tile;
+    std::vector<Wall> walls;
+    glm::vec3 position;
+
+    Corridor(glm::vec3 position, Mesh* tileMesh, Mesh* wallMesh);
+    void Draw(Camera& camera, Shader& shader) override;
+};
+
+
+#endif
